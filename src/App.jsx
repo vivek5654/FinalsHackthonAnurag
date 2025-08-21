743fe0b57e857acf69954d8489b39e1f6639843b
Vivek Vardhan <anapalavivekvardhan5654@gmail.com>
2025-08-21 06:17:15 +0530

    vivek vardhan

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-4xl'>vivek vardhan</h1>
+    </div>
+  )
+}
+
+export default App
